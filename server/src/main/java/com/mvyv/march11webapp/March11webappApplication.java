556e8489e6e1a1cf4d15ea8ce11734d5ecd34f15
yuriyvalkiv <yurivalkiv@gmail.com>
2019-03-12 11:30:21 +0100

    server side

---
+++ server/src/main/java/com/mvyv/march11webapp/March11webappApplication.java
@@ -0,0 +1,13 @@
+package com.mvyv.march11webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class March11webappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(March11webappApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/com/mvyv/march11webapp/March11webappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mvyv.march11webapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class March11webappApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
