0a126f77d8d6c613f2dacbbcbd97e04cb23f83d0
MichaelValkiv <mishavalkiv@gmail.com>
2019-03-13 14:16:06 +0200

    Added routing.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {NewsComponent} from "./news/news.component";
+import {AboutComponent} from "./about/about.component";
+import {ProductsComponent} from "./products/products.component";
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'news', component: NewsComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { NewsComponent } from './news/news.component';
 import { ProductsComponent } from './products/products.component';
+import { HomeComponent } from './home/home.component';
+import { ClarityModule } from "@clr/angular";
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
     NewsComponent,
-    ProductsComponent
+    ProductsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ClarityModule
   ],
   providers: [],
   bootstrap: [AppComponent]
