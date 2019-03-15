37a79e0e569061e7c362bfb4a66c98fa2f71e6a1
MichaelValkiv <mishavalkiv@gmail.com>
2019-03-15 22:10:55 +0200

    Pages

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { TranslateService } from "./services/translate.service";
 
 @Component({
@@ -6,13 +6,21 @@ import { TranslateService } from "./services/translate.service";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
-  constructor( private translate: TranslateService) {
-      console.log(translate.data);
+  navLinkTitle: string;
+
+  constructor( private translate: TranslateService) {}
+
+  ngOnInit() {
+    // this.activeNavLink('home');
   }
 
   setLanguage(lang: string) {
     this.translate.use(lang);
+  }
+
+  activeNavLink(linkTitle: string) {
+    this.navLinkTitle = linkTitle;
   }
 }
