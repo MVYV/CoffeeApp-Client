e088db8f2dc78dc624e83944f7017964b3eea3c6
MichaelValkiv <mishavalkiv@gmail.com>
2019-05-15 12:01:17 +0300

    Delete and modify user from admin page

---
+++ client/src/app/models/roles.model.ts
@@ -0,0 +1,5 @@
+export class Role {
+  constructor (
+    public id: number,
+    public role: string ) { }
+}
--- client/src/app/models/users.model.ts
+++ client/src/app/models/users.model.ts
@@ -1,7 +1,10 @@
 export class User {
   constructor (
-    public name: string,
-    public lastName: string,
-    public email: string,
-    public password: string ) { }
+    public id: number = 0,
+    public name: string = '',
+    public lastName: string = '',
+    public email: string = '',
+    public password: string = '',
+    public role: string = '',
+    public active: number = 0) { }
 }
